2a8ef15e07cb9e1c2a5cc0da5fa97a31e71d9f54
Hal020308 <1548011970@qq.com>
2024-06-28 18:29:39 +0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://github.com/houailing/houailing-www.dog.git',
+    base: '',
+    vite: {
+        plugins: [
+          {
+            name: 'configure-response-headers',
+            configureServer(server) {
+              server.middlewares.use((req, res, next) => {
+                res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0');
+                next();
+              });
+            },
+          },
+        ],
+      },
+});
