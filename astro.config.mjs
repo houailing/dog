e7e1602ad0d5b5904a270887007c8bb4aae7bce7
houailing <Hal020308>
2024-07-01 17:27:18 +0800

    11

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/houailing/Hal.github.io-www.dog.git',
+    site: 'https://houailing.github.com',
     base: '',
     vite: {
         plugins: [
