521880703dc2fb30350741f16595c11b815443a4
houailing <Hal020308>
2024-07-01 19:03:23 +0800

    修改

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://houailing.github.io/dog',
-    base: '',
+    site: 'https://houailing.github.io',
+    base: 'dog',
     vite: {
         plugins: [
           {
